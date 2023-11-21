4b3ed2422c4d9cc40495926db2bcd935ff40fe95
Ben Mallett <ben@Bens-MacBook-Air.local>
2023-11-20 22:23:06 -0500

    first commit to server

---
+++ app.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from 'express';
+import cors from "cors";
+
+import Hello from "./hello.js";
+import Lab5 from './lab5.js';
+import CourseRoutes from "./courses.js";
+import ModuleRoutes from "./modules.js";
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+
+Hello(app)
+Lab5(app)
+CourseRoutes(app)
+ModuleRoutes(app)
+
+app.listen(process.env.PORT || 4000)
